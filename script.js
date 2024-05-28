f54700bed17adbf075eff4dc50ca8af43b3043ef
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-28 13:00:11 +0200

    aggiunte classi tramite vue ad h1 e img

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ data() {
     return {
     titolo: 'Lezione su Vue.js',
 
-    imgUrl: "https://www.laramind.com/blog/wp-content/uploads/2019/09/Untitled-design-2019-09-03T162841.927.png"
+    imgUrl: "https://www.laramind.com/blog/wp-content/uploads/2019/09/Untitled-design-2019-09-03T162841.927.png",
+
+    miaClasse: "classeH1",
+
+    classeImg: "classeImgFilter"
     }
 }
 }).mount('#app')
