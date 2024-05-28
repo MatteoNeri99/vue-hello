8234e186bc87dcbdf9c6d0860b2df5a23c8688d1
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-28 12:49:09 +0200

    aggiunti h1 img  con valori da vue

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+const { createApp } = Vue
+
+createApp({
+data() {
+    return {
+    titolo: 'Lezione su Vue.js',
+
+    imgUrl: "https://www.laramind.com/blog/wp-content/uploads/2019/09/Untitled-design-2019-09-03T162841.927.png"
+    }
+}
+}).mount('#app')
